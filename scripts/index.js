2d6cc82d936be55f9db629e8c5fed92b69e73b82
Lilit <liliya.truf@yandex.ru>
2021-10-31 17:29:19 -0600

    add all

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+const popup = document.querySelector('.popup'); 
+const popupClose = document.querySelector('.popup__close');
+const infoButton = document.querySelector('.profile__info-button');
+
+const formButton = document.querySelector('.popup__button'); 
+const form = document.querySelector('.popup__form');
+
+const nameField = document.querySelector('.popup__input_name');
+const nameMan = document.querySelector('.profile__info-title');
+const professionField = document.querySelector('.popup__input_profession')
+const professionMan = document.querySelector('.profile__info-subtitle')
+
+function openPopup(){
+    popup.classList.add('popup_opened')
+}
+function closePopup(){
+    popup.classList.remove('popup_opened')
+}
+
+infoButton.addEventListener('click', openPopup)
+popupClose.addEventListener('click', closePopup)
+
+function popupClickHandler(event){
+    console.log(event.target)
+    if (event.target.classList.contains('popup')){
+        closePopup()
+    }
+}
+
+popup.addEventListener('mouseup', popupClickHandler)
+
+function submitForm(event){
+    event.preventDefault();
+    nameMan.textContent = nameField.value;
+    professionMan.textContent = professionField.value;
+
+
+    closePopup();
+}
+
+form.addEventListener('submit', submitForm)
