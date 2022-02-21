d1e77230039388be8bcea67d9447612fc8da3a8d
Lilit <liliya.truf@yandex.ru>
2022-02-20 21:00:08 -0600

    change after review pr9

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -8,10 +8,10 @@ class PopupWithForm extends Popup {
         this._inputElements = this._popup.querySelectorAll('.popup__input');
         this._submitButton = this._popup.querySelector('.popup__button');
         this._initialSubmitButtonText = this._submitButton.textContent;
+        this._inputs = Array.from(this._inputElements);
     }
     
     _getInputValues(){
-        this._inputs = Array.from(this._inputElements);
         this._values ={};
         this._inputs.forEach(input => {
             this._values[input.name] = input.value;
@@ -28,10 +28,13 @@ class PopupWithForm extends Popup {
         this._submitButton.textContent = 'Сохранение...';
     }
 
+    setInitialTextForSubmitButton(){
+        this._submitButton.textContent = this._initialSubmitButtonText;
+    }
+
     close(){
         this._formElement.reset();
         super.close();
-        this._submitButton.textContent = this._initialSubmitButtonText;
     }
 }
 
--- src/components/PopupWithSubmit.js
+++ src/components/PopupWithSubmit.js
@@ -1,15 +1,22 @@
 import Popup from "./Popup.js";
 
-class PopupWithSubmit extends Popup  {
-    constructor(popupSelector){
+class PopupWithSubmit extends Popup {
+    constructor(popupSelector) {
         super(popupSelector);
         this._formElement = this._popup.querySelector('.popup__form');
     }
 
-    setSubmitListener(method)
-    {
-        this._formElement.addEventListener('submit', (e)=> method(e));
-    }   
+    setSubmitListener(method) {
+        this._removeSubmitListener();
+        this._currentEventListener = (e) => method(e);
+        this._formElement.addEventListener('submit', this._currentEventListener);
+    }
+
+    _removeSubmitListener() {
+        if (this._currentEventListener !== null && this._currentEventListener !== undefined) {
+            this._formElement.removeEventListener('submit', this._currentEventListener);
+        }
+    }
 }
 
 export default PopupWithSubmit;
